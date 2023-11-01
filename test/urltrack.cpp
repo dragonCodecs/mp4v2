072b5c962fa8f52412058d8fca58b46b1db06a69
dragonmux <git@dragonmux.network>
2023-11-01 02:48:06 +0000

    test/urltrack: Automated whitespace cleanup

--- test/urltrack.cpp
+++ test/urltrack.cpp
@@ -3,19 +3,19 @@
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
  * the License at http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS
  * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
- * 
+ *
  * The Original Code is MPEG4IP.
- * 
+ *
  * The Initial Developer of the Original Code is Cisco Systems Inc.
  * Portions created by Cisco Systems Inc. are
  * Copyright (C) Cisco Systems Inc. 2001.  All Rights Reserved.
- * 
- * Contributor(s): 
+ *
+ * Contributor(s):
  *        Dave Mackie        dmackie@cisco.com
  */
 
@@ -34,7 +34,7 @@ main(int argc, char** argv)
     if (!mp4File)
         return 1;
 
-    MP4TrackId urlTrackId = 
+    MP4TrackId urlTrackId =
 #if 0
     MP4AddTrack(mp4File, "URLF");
 #else
@@ -48,7 +48,7 @@ main(int argc, char** argv)
     for (i = 1; i <= 5; i++) {
         sprintf(url, "http://server.com/foo/bar%u.html", i);
 
-        MP4WriteSample(mp4File, urlTrackId, 
+        MP4WriteSample(mp4File, urlTrackId,
             (uint8_t*)url, strlen(url) + 1, (MP4Duration)i);
     }
 
@@ -63,7 +63,7 @@ main(int argc, char** argv)
     urlTrackId = MP4FindTrackId(mp4File, 0, MP4_CNTL_TRACK_TYPE);
 #endif
     printf("urlTrackId %d\n", urlTrackId);
-    
+
     for (i = 1; i <= 5; i++) {
         uint8_t* pSample = NULL;
         uint32_t sampleSize = 0;
@@ -74,7 +74,7 @@ main(int argc, char** argv)
             &pSample, &sampleSize, NULL, &duration);
 
         if (rc) {
-            printf("Sample %i duration %llu: %s\n", 
+            printf("Sample %i duration %llu: %s\n",
                 i, duration, pSample);
             MP4Free(pSample);
         } else {
@@ -86,4 +86,3 @@ main(int argc, char** argv)
 
     return 0;
 }
-
